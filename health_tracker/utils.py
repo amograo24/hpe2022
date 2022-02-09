af5c257263817df6f85233eb5263703c9a488e5c
kushal <kushalsai2449@gmail.com>
2022-02-09 23:42:25 +0530

    urls

--- health_tracker/utils.py
+++ health_tracker/utils.py
@@ -1,8 +1,9 @@
 from typing import Union
+import random
+import uuid
 
 import pyqrcode
-import random
-from .models import Patients, User
+from .models import Patients, User, MedWorkerRep
 
 
 def return_qr_code(uid: str, scale=5) -> Union[str, bytes]:
@@ -22,7 +23,16 @@ def gen_unique_id(aadharid: str, person: User, lower: int = 12, upper: int = 16)
     return code
 
 
+def get_hcw_vid(reg_no: str, dept: str, account: User):
+    hcw_vid = uuid.uuid4()
+    hcw_vid = str(hcw_vid)[:10]
+    while MedWorkerRep.objects.filter(hcwvid=hcw_vid):
+        hcw_vid = uuid.uuid4()
+        hcw_vid = str(hcw_vid)[:10]
+    mwr = MedWorkerRep(department=dept, reg_no=reg_no, account=account, hcwvid=hcw_vid)
+    mwr.save()
+    return hcw_vid
 
 
 if __name__ == "__main__":
-    print(gen_unique_id(5, 10))
+    print(str(uuid.uuid4())[:11])
