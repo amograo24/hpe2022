10720cd35f12e5a344ae78e9dd3171e4aa4be72d
kushal <kushalsai2449@gmail.com>
2022-03-16 00:24:52 +0530

    Refined notifs.html and mvc.py

--- health_tracker/urls.py
+++ health_tracker/urls.py
@@ -13,17 +13,17 @@ urlpatterns = [
     path('get_file/<str:wbid>/<str:name>', views.get_file, name="get_file"),
     path("myfiles", views.myfiles, name="myfiles"),
     path("health/<str:wbid>", views.health_status_function, name="health"),
-    path("auth_messages", views.notification_page, name="auth_messages"), #approval messages
-    path("search",views.search,name="search"),
-    path("delete_file/<str:wbid>/<str:name>",views.delete_file,name="delete_file"),
-    path("mydoctors_vendors",views.mydoctors_vendors,name="mydoctors_vendors"),
-    path("mypatients_customers",views.mypatients_customers,name="mypatients_customers"),
-    path("edit/<str:wbid>/<str:file_name>",views.edit_file,name="edit_file"),
-    path("remove/<str:id>",views.remove_patient_vendor,name="remove_patient_vendor"),
-    path("visit/<str:id>",views.visit_qrcode,name="visit"),
-    path("gopublic",views.go_public,name="go_public"),
-    path("goprivate",views.go_private,name="go_private"),
-    path("search_public",views.search_public_vendors,name="search_public")
+    path("auth_messages", views.notification_page, name="auth_messages"),  # approval messages
+    path("search", views.search, name="search"),
+    path("delete_file/<str:wbid>/<str:name>", views.delete_file, name="delete_file"),
+    path("mydoctors_vendors", views.mydoctors_vendors, name="mydoctors_vendors"),
+    path("mypatients_customers", views.mypatients_customers, name="mypatients_customers"),
+    path("edit/<str:wbid>/<str:file_name>", views.edit_file, name="edit_file"),
+    path("remove/<str:id>", views.remove_patient_vendor, name="remove_patient_vendor"),
+    path("visit/<str:id>", views.visit_qrcode, name="visit"),
+    path("gopublic", views.go_public, name="go_public"),
+    path("goprivate", views.go_private, name="go_private"),
+    path("search_public", views.search_public_vendors, name="search_public")
 ]
 
 urlpatterns += [path('states', views.StatesAPI, name="states"),
