1dbe26030126714e71bdd7e3323a59851a445826
Avaneesh Kumar <avaneeshk098@gmail.com>
2022-03-21 19:14:13 +0000

    styling changes

--- health_tracker/static/health_tracker/js/login/login.js
+++ health_tracker/static/health_tracker/js/login/login.js
@@ -1,6 +1,39 @@
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 $(document).ready(function(){
   var $width = $(window).width();
   if($width > 929) {
+    if(getCookie('side') === 'left'){
+      
+      $('#slideBox').animate({
+        'marginLeft' : '50%'
+      });
+      $('.topLayer').animate({
+        'marginLeft': '0'
+      });
+    }
+    else{
+      $('#slideBox').animate({
+        'marginLeft' : '0'
+      });
+      $('.topLayer').animate({
+        'marginLeft' : '100%'
+      });
+    }
     $('#goRight').on('click', function(){
       $('#slideBox').animate({
         'marginLeft' : '0'
@@ -8,6 +41,7 @@ $(document).ready(function(){
       $('.topLayer').animate({
         'marginLeft' : '100%'
       });
+      document.cookie = "side=right";
     });
     $('#goLeft').on('click', function(){
       $('#slideBox').animate({
@@ -16,9 +50,26 @@ $(document).ready(function(){
       $('.topLayer').animate({
         'marginLeft': '0'
       });
+      document.cookie = "side=left"
     });
   }
   else{
+    if(getCookie('side') === 'left'){
+      $('#slideBox').animate({
+        'marginLeft' : '0'
+      });
+      $('.topLayer').animate({
+        'marginLeft' : '100%'
+      });
+    }
+    else{
+      $('#slideBox').animate({
+        'marginLeft' : '0'
+      });
+      $('.topLayer').animate({
+        'marginLeft': '0'
+      });
+    }
     $('#goRight').on('click', function(){
       $('#slideBox').animate({
         'marginLeft' : '0'
@@ -26,6 +77,7 @@ $(document).ready(function(){
       $('.topLayer').animate({
         'marginLeft' : '100%'
       });
+      document.cookie = "side=left";
     });
     $('#goLeft').on('click', function(){
       $('#slideBox').animate({
@@ -34,6 +86,7 @@ $(document).ready(function(){
       $('.topLayer').animate({
         'marginLeft': '0'
       });
+      document.cookie = "side=right";
     });
   }
 });
