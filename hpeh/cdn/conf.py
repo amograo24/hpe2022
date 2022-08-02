1bd9721c1c754f488fd364aede6def0e4aae9826
Avaneesh Kumar <45495598+avaneeshk098@users.noreply.github.com>
2022-08-02 20:50:39 +0530

    Update conf.py

--- hpeh/cdn/conf.py
+++ hpeh/cdn/conf.py
@@ -2,7 +2,7 @@ import os
 
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "DO00QA4DJ4RPXQETADWA")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "cYQHJCcmEPLhT9iEOSfgNhp12T2urUhJrO0a7PkSYUQ")
-AWS_BUCKET_NAME = "uhi"  # Your space's name
+AWS_STORAGE_BUCKET_NAME = "uhi"  # Your space's name
 AWS_S3_ENDPOINT_URL = "https://sgp1.digitaloceanspaces.com"
 
 AWS_S3_OBJECT_PARAMETERS = {
