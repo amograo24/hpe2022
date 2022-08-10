b696e9bc559d3140d2fa664848645faffc1a68a5
kushal <kushalsai2449@gmail.com>
2022-08-10 19:35:19 +0530

    fixed particles.json

--- health_tracker/static/health_tracker/js/register/main.js
+++ health_tracker/static/health_tracker/js/register/main.js
@@ -1,3 +1,5 @@
+// Unused file, feel free to delete
+
 $(document).ready(() => {
     particlesJS.load('particles-js', '../particles.json', function() {
         console.log('callback - particles.js config loaded');
